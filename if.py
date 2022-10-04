c88c5e3845815a98bd9cfadb91484f1fe42ff208
technobit64 <78329931+technobit64@users.noreply.github.com>
2022-10-04 18:49:45 -0400

    Add files via upload

---
+++ if.py
@@ -0,0 +1,10 @@
+x = int(input("Please enter a intiger"))
+if x < 0:
+    x = 0
+    print('Negagive changed to 0.')
+elif x == 0:
+     print('Zero')
+elif x == 1:
+     print('single')
+else:
+      print('more')
